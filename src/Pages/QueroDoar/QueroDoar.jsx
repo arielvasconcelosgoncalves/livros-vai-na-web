f98f4d7e8ff00bd7a812bf10826ffad1283e36b4
Ariel Vasconcelos <ariel.goncalves@sou.unifeob.edu.br>
2025-04-01 12:48:59 -0300

    Att 01/04
    Método Post

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,7 +1,49 @@
 import s from './querodoar.module.scss'
 import livro from '../../assets/simbolo_livro.png'
+import axios from 'axios'
+import { useState } from 'react';
 
 
+
+const {titulo, setTitulo} = useState("")
+
+const capturaTitulo = (e) => {
+    setTitulo(e.target.value)
+    }
+
+const {categoria, setCategoria} = useState("")
+
+const capturaCategoria = (e) => {
+    setCategoria(e.target.value)
+    }
+
+const {autor, setAutor} = useState("")
+
+const capturaAutor = (e) => {
+    setAutor(e.target.value)
+    }
+
+    
+const {url, setUrl} = useState("")
+
+const capturaUrl = (e) => {
+    setUrl(e.target.value)
+        }
+    
+const enviarDados = async() => {
+    const urlApi = ""
+    const dadosEnviar = {
+    titulo,
+    categoria,
+    autor,
+    imagem_url
+    }
+    const envioApi = await axios.post(urlApi, dadosEnviar)
+
+    alert('Livro Enviado')
+
+    setTitulo("")
+}
 
 export default function QueroDoar(){
 
@@ -10,27 +52,27 @@ export default function QueroDoar(){
         <section className={s.section1}>
             <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
         </section>
-        <form action="" className={s.section2}>
+        <form action="" className={s.section2} onSubmit={(e)=> e.preventDefault()}>
         <section className={s.sectionimg}>
                 <img src={livro} alt="" />
                 <h1>Informações do Livro</h1>
         </section>
         <section className={s.section3}>
             <div>
-                <input type="text" name='' id='' placeholder='Título' />
+                <input type="text" name='' id='' placeholder='Título' onChange={capturaTitulo} value={titulo} />
             </div>
             <div>
-                <input type="text" name='' id='' placeholder='Categoria' />
+                <input type="text" name='' id='' placeholder='Categoria' onChange={capturaCategoria} value={categoria}/>
             </div>
             <div>
-                <input type="text" name='' id='' placeholder='Autor' />
+                <input type="text" name='' id='' placeholder='Autor' onChange={capturaAutor} value={autor}/>
             </div>
             <div>
-                <input type="text" name='' id='' placeholder='Link da Imagem' />
+                <input type="text" name='' id='' placeholder='Link da Imagem' onChange={capturaUrl} value={url}/>
             </div>
         </section>
         <section className={s.section4}>
-            <button type='Submit'>Doar</button>
+            <button type='Submit' onClick={enviarDados}>Doar</button>
         </section>
         </form>
         </main>
